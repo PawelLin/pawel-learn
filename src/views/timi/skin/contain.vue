e399e20d0e057adb8ea53bc569c0a56c089d418a
pawel <linpeiwei@f-road.com.cn>
2021-07-20 15:26:25 +0800

    test

--- src/views/timi/skin/contain.vue
+++ src/views/timi/skin/contain.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="contain">
         <div class="item" :style="{ backgroundImage: `url(/src/assets/${types[data.type]})` }">{{data.type.indexOf('0') === 0 ? data.title : ''}}</div>
-        <!-- <div class="item">{{data.title}}</div> -->
         <div v-if="data.children && data.children.length">
             <timi-skin-contain v-for="item in data.children" :data="item" :key="item.title + item.type" />
         </div>
         <div v-else>
-            <img v-for="item in data.data" :src="`/src/assets/${item.icon}`" alt="" :key="item.title + item.type">
+            <img v-for="item in data.data" src="@/assets/logo.png" :title="item.name" :key="item.name + item.type" />
         </div>
     </div>
 </template>
@@ -43,7 +42,7 @@ export default defineComponent({
     align-items: center;
     font-size: 0;
     img {
-        margin: 5px 5px 0 0;
+        // margin: 5px 5px 0 0;
         height: 38px;
     }
 }
@@ -55,7 +54,19 @@ export default defineComponent({
     text-align: center;
     background-position: center;
     background-repeat: no-repeat;
+    background-size: contain;
     // background-color: #f9c;
     // background-color: #f9c;
+}
+@media screen and (max-width:600px) {
+    .contain {
+        img {
+            height: 25.33333px;
+        }
+    }
+    .item {
+        min-width: 80px;
+        min-height: 25.33333px;
+    }
 }
 </style>
